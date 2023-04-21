ea3567c48b35d2c797ffc66ab15808418439a464
Hanna <belko.hanna.off@gmail.com>
2023-04-21 14:50:51 +0200

    Configure Cypress

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
+  },
+})
