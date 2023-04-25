0e43f4999bbab451928fc2847abc7ac0f0fd8153
Hanna <belko.hanna.off@gmail.com>
2023-04-25 20:45:34 +0200

    Table tests

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -191,7 +191,7 @@ describe('First suite', () => {
 
   })
 
-  it.only('lists and dropdowns', () => {
+  it('lists and dropdowns', () => {
 
     cy.visit('/')
 
@@ -220,6 +220,50 @@ describe('First suite', () => {
         
         if( index < 3){
           cy.wrap(dropDown).click()
+        }
+      })
+    })
+  })
+
+  it.only('tables', () => {
+
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+
+    //update an existing value
+    cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
+      cy.wrap(tableRow).find('.nb-edit').click()
+      cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+      cy.wrap(tableRow).find('.nb-checkmark').click()
+
+      cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+    })
+
+    //add a new value
+    cy.get('thead').find('.nb-plus').click()
+    cy.get('thead').find('tr').eq(2).then(tableRow => {
+      cy.wrap(tableRow).find('[placeholder="First Name"]').type('Hanna')
+      cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Saldatkina')
+      cy.wrap(tableRow).find('.nb-checkmark').click()
+    })
+
+    cy.get('tbody tr').first().find('td').then(tableColumns => {
+      cy.wrap(tableColumns).eq(2).should('contain', 'Hanna')
+      cy.wrap(tableColumns).eq(3).should('contain', 'Saldatkina')
+    })
+
+    //search
+    const ages = [20, 30, 40, 150]
+
+    cy.wrap(ages).each(age => {
+      cy.get('thead [placeholder="Age"]').clear().type(age)
+      cy.wait(500)
+      cy.get('tbody tr').each(tableRow => {
+        if(age >= 150) {
+          cy.wrap(tableRow).should('contain', 'No data found')
+        } else {
+          cy.wrap(tableRow).find('td').eq(6).should('contain', age)
         }
       })
     })
