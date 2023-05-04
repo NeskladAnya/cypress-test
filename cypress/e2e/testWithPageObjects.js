cef114ae3d30384abd9611a823b7e1b68b901922
Hanna <belko.hanna.off@gmail.com>
2023-05-04 09:30:36 +0200

    Datepicker and Form layouts tests

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,3 +1,5 @@
+import { onDatepickerPage } from "../support/page_objects/datepickerPage"
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
 import { navigateTo } from "../support/page_objects/navigationPage"
 
 describe('Test with Page Objects', () => {
@@ -12,5 +14,14 @@ describe('Test with Page Objects', () => {
     navigateTo.smartTablePage()
     navigateTo.toasterPage()
     navigateTo.tooltipPage()
+  })
+
+  it.only('should submit Inline and Basic forms and select tomorrow date in calendar', () => {
+    navigateTo.formLayoutsPage()
+    onFormLayoutsPage.submitInlineFormWithNameAndEmail('Hanna', 'test@test.com')
+    onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password123')
+    navigateTo.datepickerPage()
+    onDatepickerPage.selectCommonDatepickerDateFromToday(1)
+    onDatepickerPage.selectDatepickerWithRangeFromToday(2, 10)
   })
 })
