b35e0efcec47f4414d5af58bd11c92c0604e776b
Hanna <belko.hanna.off@gmail.com>
2023-05-04 10:06:23 +0200

    Table Page Object tests

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,6 +1,7 @@
 import { onDatepickerPage } from "../support/page_objects/datepickerPage"
 import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
 import { navigateTo } from "../support/page_objects/navigationPage"
+import { onSmartTablePage } from "../support/page_objects/smartTablePage"
 
 describe('Test with Page Objects', () => {
 
@@ -16,12 +17,18 @@ describe('Test with Page Objects', () => {
     navigateTo.tooltipPage()
   })
 
-  it.only('should submit Inline and Basic forms and select tomorrow date in calendar', () => {
+  it('should submit Inline and Basic forms and select tomorrow date in calendar', () => {
     navigateTo.formLayoutsPage()
     onFormLayoutsPage.submitInlineFormWithNameAndEmail('Hanna', 'test@test.com')
     onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password123')
     navigateTo.datepickerPage()
     onDatepickerPage.selectCommonDatepickerDateFromToday(1)
     onDatepickerPage.selectDatepickerWithRangeFromToday(2, 10)
+  })
+
+  it.only('should update a row and create a new row in the table', () => {
+    navigateTo.smartTablePage()
+    onSmartTablePage.updateAgeByFirstName('Larry', '30')
+    onSmartTablePage.addNewRecordWithFirstAndLastName('Test', 'User')
   })
 })
